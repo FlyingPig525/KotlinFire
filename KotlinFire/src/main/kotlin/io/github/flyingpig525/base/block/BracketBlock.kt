7ddd4caca5d10740b22bfd914f90c0650cfda729
FlyingPig525 <noahmassouh@gmail.com>
2024-06-03 17:24:29 -0400

    BracketBlock

---
+++ KotlinFire/src/main/kotlin/io/github/flyingpig525/base/block/BracketBlock.kt
@@ -0,0 +1,15 @@
+package io.github.flyingpig525.base.block
+
+import io.github.flyingpig525.base.JsonData
+import io.github.flyingpig525.base.item.Item
+
+class BracketBlock<T>(val open: Boolean = true, val type: String) : JsonData, Block<T>("bracket", mutableListOf(), "") where T : Item, T : JsonData {
+	override fun getJsonData(): String {
+		return """{
+      		"id": "bracket",
+      		"direct": "${if (open) "open" else "close"}",
+      		"type": "$type"
+    	}""".trimIndent()
+	}
+
+}
