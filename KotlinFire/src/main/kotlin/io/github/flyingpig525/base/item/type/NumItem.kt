e987490d57cc188bb67f6ea9cdbf13a249bb2f9c
FlyingPig525 <66709612+FlyingPig525@users.noreply.github.com>
2025-04-09 20:12:10 -0400

    make NumItem support complex number item strings

--- KotlinFire/src/main/kotlin/io/github/flyingpig525/base/item/type/NumItem.kt
+++ KotlinFire/src/main/kotlin/io/github/flyingpig525/base/item/type/NumItem.kt
@@ -5,9 +5,9 @@ import io.github.flyingpig525.base.item.Item
 import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.json.put
 
-class NumItem(private val value: Double) : Item(ID.NUMBER) {
+class NumItem(private val value: String) : Item(ID.NUMBER) {
 
-    constructor(value: Int) : this(value.toDouble())
+    constructor(value: Number) : this(value.toString())
 
     override fun getJsonData() = buildJsonObject {
         put("name", value)
@@ -19,7 +19,9 @@ class NumItem(private val value: Double) : Item(ID.NUMBER) {
 //	""".trimIndent()
 
     companion object {
-        fun Number.toNumItem(): NumItem = NumItem(this.toDouble())
+        fun Number.toNumItem(): NumItem = NumItem(this)
         val Number.numItem get() = toNumItem()
+        fun String.toNumItem(): NumItem = NumItem(this)
+        val String.numItem get() = toNumItem()
     }
 }
