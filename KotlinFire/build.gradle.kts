5c92971763141300fa2515b51b7eff4490ffd0ff
FlyingPig525 <66709612+FlyingPig525@users.noreply.github.com>
2026-06-04 02:40:49 -0400

    update mavenpublish

--- KotlinFire/build.gradle.kts
+++ KotlinFire/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     kotlin("jvm") apply true
     kotlin("plugin.serialization") apply true
 
-    id("com.vanniktech.maven.publish") version "0.34.0"
-    id("org.jetbrains.dokka") version "2.0.0"
+    id("com.vanniktech.maven.publish") version "0.36.0"
+    id("org.jetbrains.dokka") version "2.2.0"
 }
 
 version = "1.7.0"
